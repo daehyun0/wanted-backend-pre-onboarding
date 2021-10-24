caa59b225effdfcdb0d4d86d869d3f5e35991ab6
DaehyunJoo <dd70296@gmail.com>
2021-10-24 14:13:52 +0900

    test(package.json): Sequelize 학습 테스트 추가
    - Insert, Select, Update, Delete 관련 테스트 추가

---
+++ model/user.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/sequelize');
+
+const User = sequelize.define('User', {
+    pk: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true
+    },
+    id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    nickname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+}, {
+    timestamps: false
+});
+
+const UserPromise = new Promise(async resolve => {
+    await User.sync({ force: true })
+    resolve(User);
+})
+
+module.exports = UserPromise;
---
+++ test/sequelize-learn.test.js
@@ -0,0 +1,73 @@
+let user = null;
+
+beforeEach((done) => {
+    user = require("../model/user.js");
+    user.then(async userDao => {
+        user = userDao;
+        await user.sync({ force: true });
+        done();
+    })
+})
+
+test('insert', async () => {
+    // given, when
+    await user.create({
+        nickname: 'jane',
+        id: 'jane',
+        password: 'asdf'
+    })
+    const users = await user.findAll();
+
+    // then
+    expect(users.length).toEqual(1);
+});
+
+test('update', async () => {
+    // given
+    await user.create({
+        nickname: 'jane',
+        id: 'jane',
+        password: 'asdf'
+    })
+
+    const jane2 = await user.create({
+        nickname: 'jane2',
+        id: 'jane2',
+        password: 'asdf2'
+    })
+
+    // when
+    jane2.nickname = 'jane2_updated';
+    await jane2.save();
+
+    // then
+    const expected = await user.findAll({
+        where: {
+            id: 'jane2'
+        }
+    });
+    expect(expected[0].nickname).toEqual('jane2_updated');
+});
+
+test('delete', async () => {
+    // given
+    await user.create({
+        nickname: 'jane',
+        id: 'jane',
+        password: 'asdf'
+    })
+
+    const jane2 = await user.create({
+        nickname: 'jane2',
+        id: 'jane2',
+        password: 'asdf2'
+    })
+
+    // when
+    await jane2.destroy();
+
+    // then
+    const users = await user.findAll();
+    expect(users.length).toEqual(1);
+    expect(users[0].id).toEqual('jane');
+})
