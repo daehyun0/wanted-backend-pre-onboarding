6b1f9c5316694d34d10637b8b34715d24482677a
DaehyunJoo <dd70296@gmail.com>
2021-10-25 22:05:24 +0900

    feat: article router 작성
    - create, read, update, delete, read list 구현

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/user');
+var userRouter = require('./routes/user');
+const articleRouter = require('./routes/article');
 
 var app = express();
 
@@ -20,8 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/user', usersRouter);
-app.use('/article', articlesRouter);
+app.use('/user', userRouter);
+app.use('/article', articleRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/article.js
@@ -0,0 +1,93 @@
+var express = require('express');
+const articleService = require("../service/article-service");
+const userService = require("../service/user-service");
+const NotAuthenticatedError = require("../error/not-authentication-error");
+var router = express.Router();
+
+router.get('/', async function(req, res, next) {
+    const pageNum = req.query.pageNum || 1;
+    const countPerPage = req.query.count || 10;
+
+    try {
+        const articlesList = await articleService.readList(pageNum, countPerPage);
+        const articleNicknameList = await Promise.all(
+            articlesList.map(async article => (await userService.findByPk(article.userPk)).nickname)
+        )
+
+        const responseArticleList = [];
+        for (let i = 0; i < articlesList.length; ++i) {
+            const article = articlesList[i];
+            responseArticleList.push({
+                no: article.pk,
+                title: article.title,
+                body: article.body,
+                writer: articleNicknameList[i],
+                createdAt: article.createdAt
+            });
+        }
+        res.send(responseArticleList);
+    } catch (e) {
+        res.send(e.message)
+    }
+});
+
+router.get('/:id', async function (req, res) {
+    const articlePk = req.params.id;
+    try {
+        const article = await articleService.read(articlePk)
+        const writer = (await userService.findByPk(article.userPk)).nickname;
+        res.send({
+            no: article.pk,
+            title: article.title,
+            body: article.body,
+            writer,
+            createdAt: article.createdAt
+        });
+    } catch (e) {
+        res.send(e.message);
+    }
+});
+
+router.post('/', async function (req, res) {
+    const jwtToken = req.cookies.access_token;
+    const title = req.body.title;
+    const body = req.body.body;
+    try {
+        const article = await articleService.write(jwtToken, title, body);
+        res.send({
+            articlePk: article.pk
+        });
+    } catch (e) {
+        res.send(e.message);
+    }
+});
+
+router.patch('/:id', async function (req, res) {
+    const articlePk = req.params.id;
+    const jwtToken = req.cookies.access_token;
+    const title = req.body.title;
+    const body = req.body.body;
+    try {
+        const article = await articleService.update(jwtToken, articlePk, title, body);
+        res.send({
+            articlePk: article.pk
+        });
+    } catch (e) {
+        res.send(e.message);
+    }
+});
+
+router.delete('/:id', async function (req, res) {
+    const articlePk = req.params.id;
+    const jwtToken = req.cookies.access_token;
+    try {
+        await articleService.delete(jwtToken, articlePk);
+        res.status(204);
+        res.send();
+    } catch (e) {
+        res.status()
+        res.send(e.message);
+    }
+});
+
+module.exports = router;
